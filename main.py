2632236edfe8f3a3695c58d6c7a1ca91ab1d74b3
chamzasd <chamzas95@gmail.com>
2020-09-10 10:26:31 -0500

    added main function

---
+++ main.py
@@ -0,0 +1,18 @@
+cols = 4
+rows = 4
+
+
+def printMaze(maze):
+    print (maze)
+    
+
+
+if __name__ == "__main__":
+    print ("Hello MrRobot")
+    print("-----")
+    f = open("defaultMaze.txt", "r")
+    #print(f.read())
+    realMaze = f.read()
+
+
+
